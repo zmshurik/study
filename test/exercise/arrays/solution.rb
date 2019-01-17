ed37538a2ba44910e2496e33ff226df16f941f73
Alexander Kurakov <zmshurik@yandex.ru>
2019-01-17 22:57:34 +0700

    solve problems

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,21 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        array.map { |item| item < 0 ? item : max }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        return -1 if array.empty?
+        first_element_index = 0
+        last_element_index = array.length - 1
+        while last_element_index >= first_element_index
+          index = (first_element_index + last_element_index) / 2
+          return index if array[index] == query
+          first_element_index = index + 1 if array[index] < query
+          last_element_index = index - 1 if array[index] > query
+        end
+        -1
       end
     end
   end
