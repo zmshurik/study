7d9e682801cc2db0162208577a0e11746904930f
Alexander Kurakov <zmshurik@yandex.ru>
2019-01-18 18:05:56 +0700

    little refactoring

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -3,13 +3,12 @@ module Exercise
     class << self
       def replace(array)
         max = array.max
-        array.map { |item| item < 0 ? item : max }
+        array.map { |item| item.negative? ? item : max }
       end
 
       def search(array, query)
-        return -1 if array.empty?
         first_element_index = 0
-        last_element_index = array.length - 1
+        last_element_index = array.size - 1
         while last_element_index >= first_element_index
           index = (first_element_index + last_element_index) / 2
           return index if array[index] == query
