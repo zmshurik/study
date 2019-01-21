6fb597870eb062c2b41b2b4107f9e18647f419e3
Alexander Kurakov <zmshurik@yandex.ru>
2019-01-21 20:51:21 +0700

    solve rackup

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,26 @@
 module Inatra
   class << self
-    def routes
+    def routes(&block)
+      module_exec(&block)
     end
 
     def call(env)
+      request = Rack::Request.new(env)
+      method = request.request_method
+      path = request.path
+      get_handler(path, method).call
+    end
+
+    private
+
+    def get_handler(path, method)
+      needed_route = @routes_map.find { |route| route[:method] == method && route[:path] == path }
+      needed_route[:handler] || -> { [404, {}, ''] }
+    end
+
+    def get(path, &block)
+      @routes_map ||= []
+      @routes_map << { method: 'GET', path: path, handler: block }
     end
   end
 end
